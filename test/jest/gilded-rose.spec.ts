2c4887d3999ef5ebc7d5f60176e176574cdb5704
Nay Oo Kyaw <nayookyaw.nok@gmail.com>
2024-01-16 22:21:54 +0800

    fix the first test case in jest

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -4,6 +4,8 @@ describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+
+    // I fixed based on the Item input
+    expect(items[0].name).toBe('foo');
   });
 });
