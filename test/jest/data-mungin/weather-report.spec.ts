66a29d7ec3e2a903eb25546c0e4340676107d4b6
Nay Oo Kyaw <nayookyaw.nok@gmail.com>
2024-01-20 18:23:48 +0800

    remove jest config js and vitest js, change the folder path

--- test/jest/data-mungin/weather-report.spec.ts
+++ test/jest/data-mungin/weather-report.spec.ts
@@ -4,13 +4,13 @@ describe('getDayWithSmallestSpread with data', () => {
     it('should return the correct day with the smallest temperature spread', () => {
         const testWeatherDataList : WeatherData[] = [
             // weather data format WeatherData { day: 1, maxTemperature: 30, minTemperature: 20 }
-            new WeatherData(1, 50, 20),
-            new WeatherData(2, 40, 35),
-            new WeatherData(3, 60, 59)
+            new WeatherData("1", 50, 20),
+            new WeatherData("2", 40, 35),
+            new WeatherData("3", 60, 59)
         ]
 
         const weatherReport : WeatherReport = new WeatherReport();
-        const dayWithSmallestSpread : number | null = weatherReport.getDayWithSmallestSpread(testWeatherDataList);
+        const dayWithSmallestSpread : string | null = weatherReport.getDayWithSmallestSpread(testWeatherDataList);
 
         // expect 3 since 60-59=1 is the smallest number
         expect(dayWithSmallestSpread).toBe(3);
@@ -22,7 +22,7 @@ describe('getDayWithSmallestSpread null', () => {
         const testWeatherDataList : WeatherData[] = []
 
         const weatherReport : WeatherReport = new WeatherReport();
-        const dayWithSmallestSpread : number | null = weatherReport.getDayWithSmallestSpread(testWeatherDataList);
+        const dayWithSmallestSpread : string | null = weatherReport.getDayWithSmallestSpread(testWeatherDataList);
 
         // expect null since input is []
         expect(dayWithSmallestSpread).toBeNull();
