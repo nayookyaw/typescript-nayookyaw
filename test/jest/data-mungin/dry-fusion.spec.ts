97f371d6f2762e59d99b9f61ce96bf1ecd944ead
Nay Oo Kyaw <nayookyaw.nok@gmail.com>
2024-01-21 18:35:36 +0800

    write test case for getting smallest difference using football.dat file and using common method file

--- test/jest/data-mungin/dry-fusion.spec.ts
+++ test/jest/data-mungin/dry-fusion.spec.ts
@@ -1,11 +1,11 @@
-import { WeatherReportCommon } from "@/data-mungin/dry-fusion-report";
+import { SoccerReportCommon, WeatherReportCommon } from "@/data-mungin/dry-fusion-report";
+import { SoccerData } from "@/data-mungin/soccer-report";
 import { WeatherData } from "@/data-mungin/weather-report";
 import {join} from "path";
 
 describe("get weather data dat file using common class", () => {
-    test ('should return the correct weather data', () => {
+    test ('should return the correct smallest temperature spread data', () => {
         const weatherCommon : WeatherReportCommon = new WeatherReportCommon();
-        console.log (__dirname);
         const weatherFilePath = join(__dirname, '../../../app/resources/weather.dat');
         const smallestWeatherData = weatherCommon.readDataFromFile(weatherFilePath, WeatherData, 2, {
             "validLength" : 3,
@@ -16,5 +16,21 @@ describe("get weather data dat file using common class", () => {
 
         // expect 14
         expect(smallestWeatherData).toBe("14")
+    });
+});
+
+describe("get soccer data dat file using common class", () => {
+    test ('should return the correct smallest difference data', () => {
+        const soccerCommon : SoccerReportCommon = new SoccerReportCommon();
+        const soccerFilePath = join(__dirname, '../../../app/resources/football.dat');
+        const smallestWeatherData = soccerCommon.readDataFromFile(soccerFilePath, SoccerData, 1, {
+            "validLength" : 8,
+            "firstColumnIndex" : 1,
+            "secondColumnIndex" : 6,
+            "thirdColumnIndex" : 8,
+        });
+
+        // expect Leicester
+        expect(smallestWeatherData).toBe("Leicester")
     });
 });
